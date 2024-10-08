ec1701dca78da6314e96efac33c055c74bfb4e8b
Raunak <raunakrajadh@gmail.com>
2024-10-08 09:47:05 +0000

    first

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('ready', async () => {
         await channel.sendSlash('302050872383242240', 'bump')
         console.count('Bumped!')
     }
-
+    
     function loop() {
         // send bump message every 2-3 hours, to prevent detection.
         var randomNum = Math.round(Math.random() * (9000000 - 7200000 + 1)) + 7200000
@@ -31,6 +31,23 @@ client.on('ready', async () => {
     
     bump()
     loop()
+
+    const mem = await client.channels.fetch('1287073269853458572')
+
+    async function memCmd() {
+        await mem.send('!djoin 935046749498789979')
+        console.count('MemberCMD!')
+    }
+
+    function lp(){
+        var randomN = Math.round(Math.random() * (210000 - 330000 + 1)) + 330000
+        setTimeout(function () {
+            memCmd()
+            lp()
+        }, randomN)
+    }
+
+    lp()
 })
 
 client.login('MTI4MzM5MjMxMzA3MDE5NDc3MQ.G01o3k.lzL6pmEaWIM0i-sgld5AFDRsp_iB8Brj_qsFLA');
